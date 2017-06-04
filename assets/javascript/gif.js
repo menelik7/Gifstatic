b1892194338f49febb6f11f9e8bbf214c98b219a
Menelik Tefera <menelikworku@gmail.com>
2017-06-04 19:16:09 -0400

    Team work is better than one mind

---
+++ assets/javascript/gif.js
@@ -0,0 +1,91 @@
+$(document).ready(function(){
+
+var topics = ["Chritiano Ronaldo", "Lionel Messi", "Neymar", "Zlatan Ibrahimović", "Luis Suárez", "Gareth Bale", "Arjen Robben", "Paul Pogba", "Sergio Ramos", "Didier Drogba"];
+
+
+	function renderButtons() {
+
+        $("#playerButtons").empty();
+
+        for (var i = 0; i < topics.length; i++) {
+			var a = $("<button>");
+			a.addClass("btn btn-primary newButton");
+			a.attr("data-name", topics[i]);
+			a.text(topics[i]);
+			$("#playerButtons").append(a);
+        }
+    };
+
+
+
+	$("#add-player").on("click", function(event) {
+
+			event.preventDefault();
+			var player = $("#topic").val().trim();
+
+			if (player == ""){
+		      return false;
+		    }
+
+			topics.push(player);
+			renderButtons();
+			return false;
+	});
+
+			renderButtons();
+
+	function displayPlayer(){
+
+	  		$("#playerLounge").empty();
+	        
+	        var category = $(this).attr("data-name");
+	        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + category + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+	        $.ajax({
+	            url: queryURL,
+	            method: "GET"
+	        })
+
+	        .done(function(response) {
+
+				var results = response.data;
+
+					for (var i = 0; i < results.length; i++) {
+						var gifDiv = $("<div class='item'>");
+						var rating = results[i].rating;
+						var p = $("<p>").text("Rating: " + rating);
+
+						var gifImage = $("<img>");
+						gifImage.attr("src", results[i].images.fixed_height.url);
+			   			gifImage.attr("src", results[i].images.fixed_height_small_still.url);
+			            gifImage.attr("data-still", results[i].images.fixed_height_small_still.url); 
+			            gifImage.attr("data-animate", results[i].images.fixed_height_small.url)
+			            gifImage.attr("data-state", "still"); 
+			            gifImage.attr("alt", topics[i] + " Gif")
+			            gifImage.addClass("image");
+
+            			gifDiv.append(gifImage);
+						gifDiv.prepend(p);
+						gifDiv.prepend(gifImage);
+						$("#playerLounge").prepend(gifDiv);
+					}
+
+	        });
+	}
+
+
+	$(document).on("click", ".image", function(){
+    var state = $(this).attr('data-state');
+    if ( state == 'still'){
+        $(this).attr('src', $(this).data('animate'));
+        $(this).attr('data-state', 'animate');
+    }else{
+        $(this).attr('src', $(this).data('still'));
+        $(this).attr('data-state', 'still');
+    }
+    });
+
+
+	$(document).on("click", ".newButton", displayPlayer);
+
+});
