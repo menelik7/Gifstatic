3371afc5a1dbee9dbc5af3f567bc847ddf9e561d
menelik7 <menelikworku@gmail.com>
2017-06-05 02:21:15 -0400

    Update gif.js

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -20,7 +20,7 @@ var topics = ["Rafa Nadal", "Roger Federer", "Novak Djokovic", "Chritiano Ronald
 
 	$("#add-player").on("click", function(event) {
                         
-		        $("#input").empty();
+		        $("#topic").empty();
 			event.preventDefault();
 			var player = $("#topic").val().trim();
 
